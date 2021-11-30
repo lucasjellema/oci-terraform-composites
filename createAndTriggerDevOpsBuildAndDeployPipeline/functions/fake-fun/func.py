9dfcae28a353db4c89df1ada885ba9f025f9f618
Lucas Jellema <lucasjellema@gmail.com>
2021-11-30 14:52:37 +0100

    Adding Function to Build into Container Image and build spec

---
+++ createAndTriggerDevOpsBuildAndDeployPipeline/functions/fake-fun/func.py
@@ -0,0 +1,18 @@
+import io
+import json
+
+from fdk import response
+
+def handler(ctx, data: io.BytesIO=None):
+    name = "World"
+    try:
+        body = json.loads(data.getvalue())
+        name = body.get("name")
+    except (Exception, ValueError) as ex:
+        print(str(ex))
+
+    return response.Response(
+        ctx, response_data=json.dumps(
+            {"message": "Hello {0}".format(name)}),
+        headers={"Content-Type": "application/json"}
+    )
