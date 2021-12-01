05a452869d9270bba5a5b18d82b0bc427ef85cf2
Lucas Jellema <lucasjellema@gmail.com>
2021-12-01 09:21:05 +0100

    -

--- createAndTriggerDevOpsBuildAndDeployPipeline/functions/fake-fun/smoke-test/datasources.tf
+++ createAndTriggerDevOpsBuildAndDeployPipeline/functions/fake-fun/smoke-test/datasources.tf
@@ -3,17 +3,18 @@ data "oci_identity_tenancy" "tenant_details" {
   tenancy_id = var.tenancy_ocid
 }
 
-data "oci_identity_regions" "home_region" {
-  filter {
-    name   = "key"
-    values = [data.oci_identity_tenancy.tenant_details.home_region_key]
-  }
-}
+#data "oci_identity_regions" "home_region" {
+#  filter {
+#    name   = "key"
+#    values = [data.oci_identity_tenancy.tenant_details.home_region_key]
+#  }
+# }
+
 output "tenancy_ocid" {
   value = var.tenancy_ocid
 }
 output "tenancy_details" {
-  value = data.oci_identity_regions.home_region
+  value = data.oci_identity_tenancy.tenant_details
 }
 
 data "oci_identity_tenancy" "oci_tenancy" {
