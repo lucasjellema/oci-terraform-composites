67f9a60f36398385ff57c752815da2dcf195f5cb
Lucas Jellema <lucasjellema@gmail.com>
2021-12-01 09:36:05 +0100

    show all devops projects in TF plan

--- createAndTriggerDevOpsBuildAndDeployPipeline/functions/fake-fun/smoke-test/datasources.tf
+++ createAndTriggerDevOpsBuildAndDeployPipeline/functions/fake-fun/smoke-test/datasources.tf
@@ -1,20 +1,11 @@
-# Gets home and current regions
-data "oci_identity_tenancy" "tenant_details" {
-  tenancy_id = var.tenancy_ocid
-}
-
-#data "oci_identity_regions" "home_region" {
-#  filter {
-#    name   = "key"
-#    values = [data.oci_identity_tenancy.tenant_details.home_region_key]
-#  }
-# }
-
 output "tenancy_ocid" {
   value = var.tenancy_ocid
 }
-output "tenancy_details" {
-  value = data.oci_identity_tenancy.tenant_details
+output "tenancy" {
+  value = data.oci_identity_tenancy.oci_tenancy
+}
+data "oci_identity_tenancy" "oci_tenancy" {
+  tenancy_id = var.tenancy_ocid
 }
 
 data "oci_identity_tenancy" "oci_tenancy" {
@@ -22,4 +13,16 @@ data "oci_identity_tenancy" "oci_tenancy" {
 }
 
 
+data "oci_devops_projects" "current_devops_projects" {
+    compartment_id = "ocid1.compartment.oc1..aaaaaaaacsssekayq4d34nl5h3eqs5e6ak3j5s4jhlws6oxf7rr5pxmt3zrq"
+ }
+
+
+locals {
+  devops_projects = data.oci_devops_projects.current_devops_projects.project_collection
+}
+
+output "devops_project_id" {
+  value = "${local.devops_projects}"
+}
 
